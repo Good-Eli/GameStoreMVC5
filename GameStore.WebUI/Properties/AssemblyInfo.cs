ccd2d868b30c2e2b94cfdf499a1213efd5d5b3bc
Lykov <v.sioffo@gmail.com>
2020-09-01 19:22:22 +0300

    Everything works. But this is not accurate

--- GameStore.WebUI/Properties/AssemblyInfo.cs
+++ GameStore.WebUI/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameStore.WebUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GameStore")]
+[assembly: AssemblyDescription("Lykov")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameStore.WebUI")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("GameStore")]
+[assembly: AssemblyCopyright("Lykov © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
